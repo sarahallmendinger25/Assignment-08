afb00bb9dc8b2da0b44adfcbb536cbc44c8d4f27
Sarah Allmendinger <sarahallmendinger1@mail.weber.edu>
2024-10-22 16:27:19 -0600

    halp

---
+++ bort.js
@@ -0,0 +1,17 @@
+document.getElementById("showsen").onclick = function(){
+    let numRows = document.getElementById("senum").value;
+    let sentence = document.getElementById("sentence").textContent;
+    const tbl = document.createElement("table");
+    while(numRows >= 0){
+        let tr = document.createElement("tr");
+        let td = document.createElement(td);
+       
+
+
+    }
+
+    document.getElementById("results").appendChild(tbl);
+
+
+
+}
