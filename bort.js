464087d4ad5c1ed2d1c9f4cd6652dfa0c792fcde
Sarah Allmendinger <sarahallmendinger1@mail.weber.edu>
2024-10-22 19:39:44 -0600

    final

--- bort.js
+++ bort.js
@@ -1,17 +1,16 @@
 document.getElementById("showsen").onclick = function(){
     let numRows = document.getElementById("senum").value;
-    let sentence = document.getElementById("sentence").textContent;
-    const tbl = document.createElement("table");
-    while(numRows >= 0){
-        let tr = document.createElement("tr");
-        let td = document.createElement(td);
-       
-
-
+    let sentence = document.getElementById("sentence").value;
+ 
+    while(numRows > 0){
+        const write = document.createElement("p");
+        write.innerHTML = sentence;
+        
+        document.getElementById("results").appendChild(write);
+        console.log(numRows);
+        numRows--;
     }
 
-    document.getElementById("results").appendChild(tbl);
-
-
+    
 
 }
